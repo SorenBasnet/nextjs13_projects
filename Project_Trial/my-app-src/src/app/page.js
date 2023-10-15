2cc8a5bee230f6d9abde621aa7d8dc1b39cc852d
SorenBasnet <basnetsoren01@gmail.com>
2023-10-14 23:53:40 -0500

    some progress

---
+++ Project_Trial/my-app-src/src/app/page.js
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Travel Bucketlist : </h1>
+          <br/>
+          <form>
+            <div className='p-10'>
+            <label>Place Name : </label>
+            <input className='text-black'></input>
+            </div>
+            <br />
+            <div className='p-10'>
+            <label>What season are you planning to travel threre at ? : </label>
+            <input className='text-black'></input>
+            </div>
+            <br />
+            <div className='p-10'>
+            <label>Budget available for the travel :</label>
+            <input className='text-black'></input>
+            </div>
+            <br />
+            <div className='p-10'>
+            <button type="button" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"><Link href={"/pages/about"}>Submit</Link></button>
+            </div>
+            </form>
+
+
+     
+      
+     
+
+    </main>
+  )
+}
